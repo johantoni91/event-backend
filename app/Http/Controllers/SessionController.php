f902e49cd668e74c87a5c08666533d69195c77e3
johantoni91 <johantoni91@gmail.com>
2023-12-11 09:16:41 +0700

    Second

--- app/Http/Controllers/SessionController.php
+++ app/Http/Controllers/SessionController.php
@@ -3,8 +3,41 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
 
 class SessionController extends Controller
 {
-    //
+    private $table_session = 'sessions';
+    private $table_event_session = 'event_sessions';
+    private $table_attendance = 'attendances';
+
+    function getSession()
+    {
+        $sessions = DB::table($this->table_session)->all();
+        if ($sessions) {
+            return response()->json($sessions, 200);
+        } else {
+            return response()->json('Failed get sessions table', 400);
+        }
+    }
+
+    function getEventSession()
+    {
+        $event_session = DB::table($this->table_event_session)->all();
+        if ($event_session) {
+            return response()->json($event_session, 200);
+        } else {
+            return response()->json('Failed get event sessions table', 400);
+        }
+    }
+
+    function getAttendance()
+    {
+        $attendance = DB::table($this->table_attendance)->all();
+        if ($attendance) {
+            return response()->json($attendance, 200);
+        } else {
+            return response()->json('Failed get attendances table', 400);
+        }
+    }
 }
